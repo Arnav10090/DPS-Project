300edffc8fbe99a29b077f3336508b5f66fe1275
Arnav Tiwari <arnavt292@gmail.com>
2025-12-11 11:47:41 +0530

    updated frontend

--- client/pages/PermitDetails.tsx
+++ client/pages/PermitDetails.tsx
@@ -1639,7 +1639,7 @@ export default function CreatePermit() {
         {showPreviewModal && (
           <div className="fixed inset-0 z-50 flex items-center justify-center">
             <div
-              className="absolute inset-0 bg-black bg-opacity-40 backdrop-blur-sm"
+              className="absolute inset-0 bg-black bg-opacity-90 backdrop-blur-sm"
               onClick={handleClosePreview}
             />
 
